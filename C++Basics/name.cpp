6d866f69df8687d2f1cbedef4419a69f76bbaa9f
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-05-27 19:26:52 +0530

    Program to take name as input and display a message.

---
+++ C++Basics/name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout << "What's your name: ";
+    //cin >> name; This will only read the first word of the name
+    getline(cin, name);
+    cout << "Hello Mr/Mrs " << name;
+
+    return 0;
+}
