af53322932e624fdd1629ab9b2845d2571518c9c
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-03 08:23:51 +0530

    Program to demonstrate Typedef function in C++.

---
+++ C++Basics/typedef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+typedef int marks;
+typedef int rollno;
+
+int main()
+{
+    marks m1, m2, m3;   // Marks is a predefined data type that describes what type its variables are.
+    rollno r1, r2, r3;  // Rollno is also a predefined data type that desctibes its variables. 
+
+    // This can later be used in the code by some functions.
+    // Typedef is used for the readability in the program, not for efficiency. 
+    // Typedef defines data types that later define (succintly) their variables. 
+
+    return 0;
+}
