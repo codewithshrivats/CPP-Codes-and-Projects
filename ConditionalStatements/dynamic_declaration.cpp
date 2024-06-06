1e1e1fcbf1f0cec06e9019cbb4c74f25f2fc2cf6
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-06 13:51:38 +0530

    Program to demonstrate dynamic declaration of variables in C++.

---
+++ ConditionalStatements/dynamic_declaration.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    /* if()
+    {
+        int i;  This is called dynamic declaration. As soon as the program enters this if block 
+                variable i is assigned memory in the stack section (activation record of main function).
+                When the program exits this code block, the variables are deleted. 
+    }
+
+    if()
+    {
+        int j;
+    } 
+    
+    if(int i = 0, i < k)    This expression is also allowed. 
+    {
+        ...
+    }
+
+    */
+
+    return 0;
+}
