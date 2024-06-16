e91b350e23793d31c64a9aa8c4750dc3ff7d489e
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-16 09:14:29 +0530

    Program to display elements of an array using for each loop in C++.

---
+++ Arrays/for-each.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    int A[] = {2,4,6,8,10};
+
+    for(int x:A)
+        cout << x << " ";
+
+    cout << endl;
+
+    return 0;
+}
