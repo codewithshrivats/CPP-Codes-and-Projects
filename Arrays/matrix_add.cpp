b246baec0bd4d81cf1923c669f7ddb2472595be1
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-18 08:31:06 +0530

    Program to add 2 matrices and display the result in C++.

---
+++ Arrays/matrix_add.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[2][3] = {2,5,9,7,3,6};
+    int B[2][3] = {6,3,4,9,5,2};
+    int C[2][3];
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<3; j++)
+        {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
