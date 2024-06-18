85af81864291c43cb4933ef1e76e0f0030cd9e5d
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-18 08:32:00 +0530

    Pattern 1 in C++.

---
+++ Arrays/pattern1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 1;
+    int i, j;
+
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }    
+
+    return 0;
+}
