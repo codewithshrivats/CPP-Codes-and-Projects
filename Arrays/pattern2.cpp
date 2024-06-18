c75404c1436740101ee38afc26c25a4d7db5c7cc
Shrivats Agrawal <shrivatsagr24@gmail.com>
2024-06-18 08:32:12 +0530

    Pattern 2 in C++.

---
+++ Arrays/pattern2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            if(j<=i)
+                cout << "*";
+            else    
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            if(i<=j)
+                cout << "*";
+            else    
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
